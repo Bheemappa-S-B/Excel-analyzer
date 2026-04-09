6529ba63394917ed01664da7fde32ebb813b4751
Bheemappa <bheemappabaraker9@gmail.com>
2026-04-09 15:28:04 +0530

    api setup

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:1000/api",
+  baseURL: "https://excel-analyzer-bla0.onrender.com/",
 });
 
 export const setAuthToken = (token) => {
