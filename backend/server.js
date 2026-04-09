eafb1eced559e2b69115c595ceaa54c5ff8283db
Bheemappa <bheemappabaraker9@gmail.com>
2026-04-09 16:50:24 +0530

    cors change

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,11 @@ const app = express();
 const allowedOrigins = ["http://localhost:3000", "https://excel-analyzer-bice.vercel.app/"]
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:3000",
+      "https://excel-analyzer-bice.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
 );
